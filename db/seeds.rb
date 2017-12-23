8da71a469dae50f7fee4e1f17dc78a314efb6d97
Adam Miribyan <adam@bysestra.com>
2017-12-23 18:26:44 +0100

    Add a simple Article model and some seed data to begin with.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  'How Notifications Work',
+  'Intro to the Clientside',
+  'How do I reinvite someone or check status?',
+  '@Mentions',
+  'How do I turn notifications on/off?',
+  'Clientside Message Board',
+  'Reports',
+  'Inviting People to a Project',
+  'Adding Clients',
+  'Managing People',
+  'Tracking Bugs',
+].each do |title|
+  Article.create(title: title)
+end
